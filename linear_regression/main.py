64201729e10bac8c2300c9d9f8286720af16e483
Yeonho Jang <siabard@gmail.com>
2019-07-17 16:42:37 +0900

    선형회귀 1단계 진행 중

---
+++ linear_regression/main.py
@@ -0,0 +1,37 @@
+# minimize sum of squre error (Mean Square Error)
+# Y - Y' = Y - theta * Xt
+# theta is vector
+# X is parameter
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# loading dataset using pandas
+
+house = pd.read_csv('home_data.csv')
+
+# print head rows (default 5)
+# house.head()
+
+# print tail rows
+# house.tail()
+
+# 관련 DataFrame의 정보를 가져오기
+# house.info()
+
+# 관련 DataFrame에 대한 설명
+# house.describe()
+
+# DataFrame의 전체 컬럼
+# house.columns
+
+# Y 값은 price, Feature는 bedrooms, bathrooms 등이 될 것임
+# 해당 feature를 가지고 Y값을 추정(Predict)할 것임
+
+# 스캐터그램을 출력해본다.
+# plt.scatter(house.sqrt_living, house.price)
+# plt.xlabel('sqrt of house')
+# plt.ylabel('price of house')
+# plt.show()
